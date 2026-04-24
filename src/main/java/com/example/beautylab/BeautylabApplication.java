3245b601d7e9b82d025ad851a670b0a6cd717e49
jeimypaez08 <jeimypaez163@gmail.com>
2026-04-24 12:11:31 -0500

    Initial commit: creacion y actualizacion gitignore

---
+++ src/main/java/com/example/beautylab/BeautylabApplication.java
@@ -0,0 +1,13 @@
+package com.example.beautylab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautylabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautylabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/beautylab/BeautylabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beautylab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautylabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
